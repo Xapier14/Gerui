8007994b300885bb12c8503eb4c9467ebdf6ffaf
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-01-30 18:52:12 +0800

    Add new components (Label Panel)

---
+++ Components/Label.cs
@@ -0,0 +1,53 @@
+﻿using GEngine;
+using GEngine.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerui.Components
+{
+    public class Label : IComponent, ILabel
+    {
+        public FontResource? Font { get; set; }
+        public string Text { get; set; }
+        public ColorRGBA ForeColor { get; set; }
+        public ColorRGBA BackColor { get; set; }
+        public Coord LastDrawnPosition { get; set; }
+        public Coord Offset { get; set; }
+        public bool Visible { get; set; }
+
+        public Label()
+        {
+            Text = "";
+            Font = null;
+            ForeColor = ColorRGBA.BLACK;
+            BackColor = ColorRGBA.TRANSPARENT;
+            Offset = Coord.Zero;
+            Visible = true;
+        }
+
+        public void Draw(GraphicsEngine engine, Coord offset, object? data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent? GetHoveredComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMouseOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(WindowController window, object? data)
+        {
+            if (!Visible)
+                return;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Components/Panel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GEngine;
+using GEngine.Engine;
+using Gerui;
+
+using static GEngine.GEngine;
+
+namespace Gerui.Components
+{
+    public class Panel : IComponent, IGridable
+    {
+        public Coord LastDrawnPosition { get; set; }
+        public Coord Offset { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int RowSpan { get; set; }
+        public int ColumnSpan { get; set; }
+        public ColorRGBA BackColor { get; set; }
+        public ColorRGBA BorderColor { get; set; }
+        public HorizontalAnchor HorizontalAnchor { get; set; }
+        public VerticalAnchor VerticalAnchor { get; set; }
+        public Size Size { get; set; }
+        public bool Visible { get; set; }
+        public Panel()
+        {
+            Row = 0;
+            Column = 0;
+            RowSpan = 1;
+            ColumnSpan = 1;
+            BackColor = ColorRGBA.TRANSPARENT;
+            BorderColor = ColorRGBA.TRANSPARENT;
+            HorizontalAnchor = HorizontalAnchor.Left;
+            VerticalAnchor = VerticalAnchor.Top;
+            Size = new Size(100, 100);
+            Visible = true;
+        }
+
+        public void Draw(GraphicsEngine engine, Coord offset, object? data)
+        {
+            Size size;
+            Coord drawPosition = new(offset);
+            LastDrawnPosition = new(offset);
+            if (data is not GEngine.Size && data is not (GEngine.Size, Coord))
+            {
+                size = engine.GetInternalResolution();
+            } else
+            {
+                if (data is Coord)
+                {
+                    size = (Size)data;
+                } else
+                {
+                    size = (((Size, Coord))data).Item1;
+                    LastDrawnPosition += (((Size, Coord))data).Item2;
+                }
+                Size = new(size);
+            }
+            if (!Visible)
+                return;
+
+            if (HorizontalAnchor == HorizontalAnchor.Right)
+            {
+                drawPosition.X -= Offset.X;
+            }
+            else
+            {
+                drawPosition.X += Offset.X;
+            }
+            if (VerticalAnchor == VerticalAnchor.Bottom)
+            {
+                drawPosition.Y -= Offset.Y;
+            }
+            else
+            {
+                drawPosition.Y += Offset.Y;
+            }
+
+            engine.SetRenderDrawColor(BackColor);
+            engine.DrawRectangleFilled(drawPosition.X, drawPosition.Y,
+                                       drawPosition.X + size.W, drawPosition.Y + size.H);
+            engine.SetRenderDrawColor(BorderColor);
+            engine.DrawRectangle(drawPosition.X, drawPosition.Y,
+                                       drawPosition.X + size.W, drawPosition.Y + size.H);
+            engine.SetRenderDrawColor(ColorRGBA.WHITE);
+            engine.DrawText(Resources.GetFontResource("fnt_ui"), "This is a panel", 4, 4);
+        }
+
+        public IComponent? GetHoveredComponent()
+        {
+            return IsMouseOver() ? this : null;
+        }
+
+        public bool IsMouseOver()
+        {
+            Coord cursor = Input.WindowMouse;
+            return cursor.X >= LastDrawnPosition.X && cursor.Y >= LastDrawnPosition.Y &&
+                   cursor.X <= LastDrawnPosition.X + Size.W && cursor.Y <= LastDrawnPosition.Y + Size.H;
+        }
+
+        public void Update(WindowController window, object? data)
+        {
+
+        }
+    }
+}
