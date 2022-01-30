1f3bad4c5b37700f3ab965c11d0e3bbe4ebb8b36
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-01-30 18:31:52 +0800

    Add border color highlight. Revised click logic.

--- Components/ContextMenuItems/ContextMenuButton.cs
+++ Components/ContextMenuItems/ContextMenuButton.cs
@@ -20,6 +20,7 @@ namespace Gerui.Components
         public ColorRGBA BackColor { get; set; }
         public ColorRGBA BorderColor { get; set; }
         public ColorRGBA HighlightColor { get; set; }
+        public ColorRGBA HighlightBorderColor { get; set; }
         public bool Highlight { get; set; }
 
         public event ButtonEventHandler? OnClick;
@@ -38,9 +39,10 @@ namespace Gerui.Components
                 return new Size(size.W + Padding.X * 2, size.H + Padding.Y * 2);
             }
         }
+        public override bool Visible { get; set; }
 
         private bool _hovered = false;
-        private bool _wasDown = false;
+        private bool _wasPressed = false;
         private int _minWidth = -1;
 
         public ContextMenuButton()
@@ -51,8 +53,10 @@ namespace Gerui.Components
             BackColor = ColorRGBA.TRANSPARENT;
             BorderColor = BackColor;
             HighlightColor = new ColorRGBA(240, 240, 240, 40);
+            HighlightBorderColor = ColorRGBA.TRANSPARENT;
             Highlight = false;
             Padding = new Coord(4, 4);
+            Visible = true;
         }
 
         public override void Draw(GraphicsEngine graphics, Coord offset, object? data)
@@ -65,6 +69,9 @@ namespace Gerui.Components
                 minWidth = (int)data;
                 _minWidth = minWidth;
             }
+
+            if (!Visible)
+                return;
 
             int x1 = offset.X + Offset.X, y1 = offset.Y + Offset.Y;
             int x2 = x1 + (minWidth > 0 ? minWidth : Size.W), y2 = y1 + Size.H;
@@ -85,6 +92,8 @@ namespace Gerui.Components
             {
                 graphics.SetRenderDrawColor(HighlightColor);
                 graphics.DrawRectangleFilled(x1, y1, x2, y2);
+                graphics.SetRenderDrawColor(HighlightBorderColor);
+                graphics.DrawRectangle(x1, y1, x2, y2);
             }
         }
 
@@ -113,26 +122,29 @@ namespace Gerui.Components
 
                     });
                 }
-                if (!Input.MouseLeftButtonDown && _wasDown)
-                {
-                    OnClick?.Invoke(this, new ButtonEventArgs()
-                    {
 
-                    });
+                if (!_wasPressed && Input.MouseLeftButtonDownPressed)
+                {
+                    _wasPressed = true;
+                }
+
+                if (_wasPressed && Input.MouseLeftButtonDownReleased)
+                {
+                    _wasPressed = false;
+                    OnClick?.Invoke(this, new ButtonEventArgs());
                 }
             }
             else
             {
                 if (_hovered)
                 {
-                    Highlight = _hovered = false;
+                    Highlight = _hovered = _wasPressed = false;
                     OnMouseLeave?.Invoke(this, new ButtonEventArgs()
                     {
 
                     });
                 }
             }
-            _wasDown = Input.MouseLeftButtonDown;
         }
     }
 }
