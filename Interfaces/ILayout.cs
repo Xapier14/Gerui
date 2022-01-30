01a54fb101a05d32abeb1ef4825fbaca6b6510b2
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-01-30 18:52:29 +0800

    Add ILayout Interface

---
+++ Interfaces/ILayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerui.Interfaces
+{
+    public interface ILayout
+    {
+        public void InitializeComponents(WindowController window);
+        public void WindowLoad(object? sender, WindowController window);
+        public void Update(object? sender, WindowController window);
+    }
+}
